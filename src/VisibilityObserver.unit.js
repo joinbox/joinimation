627555784d498c100a9ab55e1d34c4df3832a56b
Felix Steiner <felix_steiner@bluewin.ch>
2021-03-19 14:04:09 +0100

    fix(visibilityobserver): reduce threshold of 20% on large elements

--- src/VisibilityObserver.unit.js
+++ src/VisibilityObserver.unit.js
@@ -6,7 +6,7 @@ const setupData = () => {
     const listeners = [];
     const emitter = {
         on: (name, fn) => listeners.push(fn),
-        emit: (data) => listeners.forEach(listener => listener(data)),
+        emit: data => listeners.forEach(listener => listener(data)),
         getOptionsForTrigger: () => ({ stagger: 50 }),
     };
     class IntersectionObserver {
@@ -29,7 +29,7 @@ test('uses correct options for IntersectionObserver', (t) => {
     const { emitter, IntersectionObserver } = setupData();
     global.IntersectionObserver = IntersectionObserver;
     const observer = new VisibilityObserver(emitter);
-    t.deepEqual(observer.intersectionObserver.options, { threshold: [0.2] });
+    t.deepEqual(observer.intersectionObserver.options, { threshold: [0.01, 0.05, 0.1, 0.2] });
     delete global.IntersectionObserver;
 });
 
@@ -43,6 +43,7 @@ test('listens to elements that were emitted by addTrigger', (t) => {
     delete global.IntersectionObserver;
 });
 
+
 test.cb('emits intersect for intersected elements', (t) => {
 
     const { emitter, IntersectionObserver } = setupData();
@@ -53,14 +54,17 @@ test.cb('emits intersect for intersected elements', (t) => {
     observer.on('intersect', el => intersectedElements.push(el));
 
     const elements = ['el1', 'el2', 'el3'];
+    // For callback see above in IntersectionObserver's fake implementation
     observer.intersectionObserver.callback(
         // Callback is called with entries that have a property 'target'
         elements.map(target => ({
             target,
             // el3 is not visible, should not fire
             isIntersecting: target !== 'el3',
+            boundingClientRect: { height: 100 },
+            intersectionRatio: 0.2,
         })),
-        { unobserve: () => {} }
+        { unobserve: () => {} },
     );
 
     // First stagger after 50ms
@@ -72,6 +76,45 @@ test.cb('emits intersect for intersected elements', (t) => {
 
     delete global.IntersectionObserver;
 });
+
+
+
+
+test('uses correct thresholds', async(t) => {
+
+    const { emitter, IntersectionObserver } = setupData();
+    global.IntersectionObserver = IntersectionObserver;
+    const observer = new VisibilityObserver(emitter);
+
+    const intersectedElements = [];
+    observer.on('intersect', el => intersectedElements.push(el));
+
+    observer.intersectionObserver.callback(
+        [
+            // Not intersecting, because 0.05 is too small for height 2000
+            {
+                target: 'el1',
+                isIntersecting: true,
+                boundingClientRect: { height: 2000 },
+                intersectionRatio: 0.05,
+            },
+            // Intersecting, because height of > 2000 uses 0.05
+            {
+                target: 'el2',
+                isIntersecting: true,
+                boundingClientRect: { height: 2001 },
+                intersectionRatio: 0.05,
+            },
+        ],
+        { unobserve: () => {} },
+    );
+
+    await new Promise(resolve => setTimeout(resolve, 50));
+    t.deepEqual(intersectedElements, ['el2']);
+
+    delete global.IntersectionObserver;
+});
+
 
 
 test('unobserves elements that were intersected', (t) => {
@@ -87,8 +130,10 @@ test('unobserves elements that were intersected', (t) => {
         elements.map(target => ({
             target,
             isIntersecting: target === 'el1',
+            boundingClientRect: { height: 100 },
+            intersectionRatio: 0.2,
         })),
-        { unobserve }
+        { unobserve },
     );
     // Element that became visible is unobserved
     t.deepEqual(unobservedElements, ['el1']);
